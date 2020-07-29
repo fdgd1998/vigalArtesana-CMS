6f0a4d0b5de35e76acd455cca48734b6ca472647
Francisco Gálvez <afgalvezdiaz@gmail.com>
2020-07-30 00:46:47 +0200

    Authentication fix

--- modules/authenticate.php
+++ modules/authenticate.php
@@ -1,11 +1,5 @@
 <?php
     session_start();
-    //error_reporting(0);
-    
-    if (!isset($_SESSION['loggedin'])) {
-        header("Location: ../403.php");
-        exit();
-    }
     
     if ($_POST) {
         $user_form = trim($_POST['user']);

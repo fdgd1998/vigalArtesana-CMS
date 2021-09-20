aa50c2169e2fc53d91f4a6a74e8de19091c8bccd
Francisco Gálvez <afgalvezdiaz@gmail.com>
2021-09-20 15:43:01 +0200

    Added cryptographic key file

---
+++ scripts/cryptographic_key.php
@@ -0,0 +1,5 @@
+<?php
+    // On production servers, this file should be stored in a non public folder
+    //$encryption_key_256bit = base64_encode(openssl_random_pseudo_bytes(32));
+    $GLOBALS['key'] = 'key removed due to security reasonss';
+?>

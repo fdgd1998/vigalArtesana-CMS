846aa9e19c10276214881012f3f1ffd35a67ae8a
Francisco Gálvez <afgalvezdiaz@gmail.com>
2021-09-20 15:39:58 +0200

    Added comment

--- scripts/cryptographic_key.php
+++ scripts/cryptographic_key.php
@@ -1,5 +1,6 @@
-/*On proudction servers, this file should be located on a non public folder*/
+
 <?php
+    /*On proudction servers, this file should be located on a non public folder*/
     //$encryption_key_256bit = base64_encode(openssl_random_pseudo_bytes(32));
     $GLOBALS['key'] = 'put the key here';
 ?>

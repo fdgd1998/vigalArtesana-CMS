9a4dfb1e46e3334f4bb64983e2e130d75cc419e5
Francisco Gálvez <afgalvezdiaz@gmail.com>
2022-05-19 01:09:34 +0200

    Update db_structure.sql

--- db_structure.sql
+++ db_structure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 18-05-2022 a las 07:40:29
+-- Tiempo de generación: 18-05-2022 a las 23:01:54
 -- Versión del servidor: 10.5.12-MariaDB-cll-lve
 -- Versión de PHP: 7.2.34
 
@@ -35,10 +35,8 @@ CREATE TABLE `categories` (
   `name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `cat_enabled` char(3) CHARACTER SET utf8mb4 DEFAULT 'YES',
   `image` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `uploadedBy` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+  `uploadedBy` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `company_info`
@@ -55,7 +53,7 @@ CREATE TABLE `company_info` (
 
 INSERT INTO `company_info` (`key_info`, `value_info`) VALUES
 ('phone', '+34 600 00 00 00'),
-('location', 'Calle Pruea, 123, 29000 Málaga'),
+('location', 'Calle Prueba, 123, 29000 Málaga'),
 ('name', 'Mi Empresa'),
 ('email', 'contacto@miempresa.es'),
 ('social_media', '{\"instagram\":\"miempresa\",\"facebook\":\"miempresa\",\"whatsapp\":\"+34600000000\"}'),
@@ -77,11 +75,8 @@ CREATE TABLE `gallery` (
   `id` int(5) NOT NULL,
   `filename` varchar(50) CHARACTER SET latin1 NOT NULL,
   `category` int(3) NOT NULL,
-  `uploadedBy` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+  `uploadedBy` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
 --
 -- Estructura de tabla para la tabla `services`
 --
@@ -92,8 +87,6 @@ CREATE TABLE `services` (
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `image` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `users`
@@ -106,9 +99,9 @@ CREATE TABLE `users` (
   `passwd` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `firstname` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
   `surname` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `account_type` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `account_enabled` char(10) CHARACTER SET utf8mb4 DEFAULT 'YES',
-  `passwd_reset` char(10) CHARACTER SET utf8mb4 DEFAULT 'NO'
+  `account_type` int(4) DEFAULT NULL,
+  `account_enabled` tinyint(1) DEFAULT 1,
+  `passwd_reset` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -116,59 +109,28 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `passwd`, `firstname`, `surname`, `account_type`, `account_enabled`, `passwd_reset`) VALUES
-(0, 'admin', 'test@miempresa.es', '$2y$10$Y8hcDbCBELzbvAVA5u3xK.7P5WA0V3GtDsL6mAD1Qi6Svdpl2fwym', NULL, NULL, 'superuser', 'YES', 'NO');
+(0, 'admin', 'test@miempresa.es', '$2y$10$Y8hcDbCBELzbvAVA5u3xK.7P5WA0V3GtDsL6mAD1Qi6Svdpl2fwym', NULL, NULL, 'superuser', 1, 1, 0);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user_permissions`
+-- Estructura de tabla para la tabla `user_roles`
 --
 
-CREATE TABLE `user_permissions` (
+CREATE TABLE `user_roles` (
   `id` int(4) NOT NULL,
-  `categories` tinyint(1) DEFAULT 0,
-  `gallery_new` tinyint(1) DEFAULT 0,
-  `manage_gallery` tinyint(1) DEFAULT 0,
-  `get_friendly_url` tinyint(1) DEFAULT 0,
-  `change_category_status` tinyint(1) DEFAULT 0,
-  `check_category_name` tinyint(1) DEFAULT 0,
-  `create_category` tinyint(1) DEFAULT 0,
-  `create_gallery_items` tinyint(1) DEFAULT 0,
-  `delete_category` tinyint(1) DEFAULT 0,
-  `delete_images` tinyint(1) DEFAULT 0,
-  `edit_category` tinyint(1) DEFAULT 0,
-  `get_categories` tinyint(1) DEFAULT 0,
-  `retrieve_cetegory_image` tinyint(1) DEFAULT 0,
-  `about_us` tinyint(1) DEFAULT 0,
-  `advanced` tinyint(1) DEFAULT 0,
-  `contact` tinyint(1) DEFAULT 0,
-  `edit_service` tinyint(1) DEFAULT 0,
-  `general` tinyint(1) DEFAULT 0,
-  `new_service` tinyint(1) DEFAULT 0,
-  `services` tinyint(1) DEFAULT 0,
-  `create_service` tinyint(1) DEFAULT 0,
-  `delete_service` tinyint(1) DEFAULT 0,
-  `update_about_us` tinyint(1) DEFAULT 0,
-  `update_contact_phone_email` tinyint(1) DEFAULT 0,
-  `update_index_brief_description` tinyint(1) DEFAULT 0,
-  `update_index_image` tinyint(1) DEFAULT 0,
-  `update_index_image_description` tinyint(1) DEFAULT 0,
-  `update_maintenance_mode` tinyint(1) DEFAULT 0,
-  `update_map_link` tinyint(1) DEFAULT 0,
-  `update_opening_hours` tinyint(1) DEFAULT 0,
-  `update_service` tinyint(1) DEFAULT 0,
-  `update_social_media` tinyint(1) DEFAULT 0,
-  `create_user` tinyint(1) NOT NULL DEFAULT 0,
-  `manage_users` tinyint(1) NOT NULL DEFAULT 0,
-  `delete_user` tinyint(1) NOT NULL DEFAULT 0
+  `role` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `permissions` bit(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Volcado de datos para la tabla `user_permissions`
+-- Volcado de datos para la tabla `user_roles`
 --
 
-INSERT INTO `user_permissions` (`id`, `categories`, `gallery_new`, `manage_gallery`, `get_friendly_url`, `change_category_status`, `check_category_name`, `create_category`, `create_gallery_items`, `delete_category`, `delete_images`, `edit_category`, `get_categories`, `retrieve_cetegory_image`, `about_us`, `advanced`, `contact`, `edit_service`, `general`, `new_service`, `services`, `create_service`, `delete_service`, `update_about_us`, `update_contact_phone_email`, `update_index_brief_description`, `update_index_image`, `update_index_image_description`, `update_maintenance_mode`, `update_map_link`, `update_opening_hours`, `update_service`, `update_social_media`, `create_user`, `manage_users`, `delete_user`) VALUES
-(0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO `user_roles` (`id`, `role`, `permissions`) VALUES
+(1, 'superuser', b'0000000011111111'),
+(2, 'administrator', b'0000000011111110'),
+(3, 'gallery_collaborator', b'0000000011111000');
 
 --
 -- Índices para tablas volcadas
@@ -178,14 +140,16 @@ INSERT INTO `user_permissions` (`id`, `categories`, `gallery_new`, `manage_galle
 -- Indices de la tabla `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `uploadedBy` (`uploadedBy`);
 
 --
 -- Indices de la tabla `gallery`
 --
 ALTER TABLE `gallery`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `category` (`category`);
+  ADD KEY `category` (`category`),
+  ADD KEY `gallery_ibfk_1` (`uploadedBy`);
 
 --
 -- Indices de la tabla `services`
@@ -197,12 +161,13 @@ ALTER TABLE `services`
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_type` (`account_type`);
 
 --
--- Indices de la tabla `user_permissions`
+-- Indices de la tabla `user_roles`
 --
-ALTER TABLE `user_permissions`
+ALTER TABLE `user_roles`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -213,19 +178,19 @@ ALTER TABLE `user_permissions`
 -- AUTO_INCREMENT de la tabla `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=215;
 
 --
 -- AUTO_INCREMENT de la tabla `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
@@ -234,14 +199,32 @@ ALTER TABLE `users`
   MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
+-- AUTO_INCREMENT de la tabla `user_roles`
+--
+ALTER TABLE `user_roles`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`uploadedBy`) REFERENCES `users` (`id`);
 
 --
 -- Filtros para la tabla `gallery`
 --
 ALTER TABLE `gallery`
-  ADD CONSTRAINT `gallery_ibfk_1` FOREIGN KEY (`category`) REFERENCES `categories` (`id`);
+  ADD CONSTRAINT `gallery_ibfk_1` FOREIGN KEY (`uploadedBy`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`account_type`) REFERENCES `user_roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

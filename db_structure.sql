046830b85ce11b03830db9318439a5afd1be89a7
Francisco Gálvez <afgalvezdiaz@gmail.com>
2022-05-18 09:46:03 +0200

    Update db_structure.sql

--- db_structure.sql
+++ db_structure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 18-05-2022 a las 07:24:48
+-- Tiempo de generación: 18-05-2022 a las 07:40:29
 -- Versión del servidor: 10.5.12-MariaDB-cll-lve
 -- Versión de PHP: 7.2.34
 
@@ -49,6 +49,24 @@ CREATE TABLE `company_info` (
   `value_info` longtext CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `company_info`
+--
+
+INSERT INTO `company_info` (`key_info`, `value_info`) VALUES
+('phone', '+34 600 00 00 00'),
+('location', 'Calle Pruea, 123, 29000 Málaga'),
+('name', 'Mi Empresa'),
+('email', 'contacto@miempresa.es'),
+('social_media', '{\"instagram\":\"miempresa\",\"facebook\":\"miempresa\",\"whatsapp\":\"+34600000000\"}'),
+('index-image', 'index.jpg'),
+('index-image-description', 'Descripción de mi empresa.'),
+('google-map-link', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1654425.3095923811!2d-4.408954431630243!3d35.91517012339416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd7134e9fd7091b5%3A0xd1256645b08b9d84!2sMar%20de%20Albor%C3%A1n!5e0!3m2!1ses!2ses!4v1652859736537!5m2!1ses!2ses'),
+('opening-hours', '{\"Lunes\":\"7:00 - 15:00\",\"Martes\":\"7:00 - 15:00\",\"Miércoles\":\"7:00 - 15:00\",\"Jueves\":\"7:00 - 15:00\",\"Viernes\":\"7:00 - 13:30\",\"Sábado\":\"cerrado\",\"Domingo\":\"cerrado\"}'),
+('about-us', 'Esta es la sección sobre nosotros de la página web.'),
+('index-brief-description', 'Esta es una breve descripción de la página web.'),
+('maintenance', 'false');
+
 -- --------------------------------------------------------
 
 --
@@ -92,6 +110,13 @@ CREATE TABLE `users` (
   `account_enabled` char(10) CHARACTER SET utf8mb4 DEFAULT 'YES',
   `passwd_reset` char(10) CHARACTER SET utf8mb4 DEFAULT 'NO'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `passwd`, `firstname`, `surname`, `account_type`, `account_enabled`, `passwd_reset`) VALUES
+(0, 'admin', 'test@miempresa.es', '$2y$10$Y8hcDbCBELzbvAVA5u3xK.7P5WA0V3GtDsL6mAD1Qi6Svdpl2fwym', NULL, NULL, 'superuser', 'YES', 'NO');
 
 -- --------------------------------------------------------
 
@@ -137,6 +162,13 @@ CREATE TABLE `user_permissions` (
   `manage_users` tinyint(1) NOT NULL DEFAULT 0,
   `delete_user` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `user_permissions`
+--
+
+INSERT INTO `user_permissions` (`id`, `categories`, `gallery_new`, `manage_gallery`, `get_friendly_url`, `change_category_status`, `check_category_name`, `create_category`, `create_gallery_items`, `delete_category`, `delete_images`, `edit_category`, `get_categories`, `retrieve_cetegory_image`, `about_us`, `advanced`, `contact`, `edit_service`, `general`, `new_service`, `services`, `create_service`, `delete_service`, `update_about_us`, `update_contact_phone_email`, `update_index_brief_description`, `update_index_image`, `update_index_image_description`, `update_maintenance_mode`, `update_map_link`, `update_opening_hours`, `update_service`, `update_social_media`, `create_user`, `manage_users`, `delete_user`) VALUES
+(0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 
 --
 -- Índices para tablas volcadas
@@ -199,7 +231,7 @@ ALTER TABLE `services`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Restricciones para tablas volcadas

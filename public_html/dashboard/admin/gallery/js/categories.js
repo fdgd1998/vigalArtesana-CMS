12bb185efdfb97d4c66962baa4df279b665fbdfd
Francisco Gálvez <afgalvezdiaz@gmail.com>
2022-10-29 21:58:31 +0200

    Removed feature
    Enabling/disabling categories

--- public_html/dashboard/admin/gallery/js/categories.js
+++ public_html/dashboard/admin/gallery/js/categories.js
@@ -26,36 +26,6 @@ $('#cat-delete').on('click', function(e) {
     });
 });
 
-$("#cat-statuschange").on('click', function(e) {
-    ShowSpinnerOverlay("Actualizando categoría...");
-    disable = 'YES';
-    if ($('#'+editing_cat_id+'_cat_status').text().trim() == 'Habilitada') {
-        console.log("la categoria sera deshabilitada");
-        disable = 'NO';
-    }
-    cat_id = editing_cat_id.substring(6);
-    console.log("category id: "+cat_id);
-    console.log("category status: "+disable);
-    $.ajax({
-        url: location.origin+'/dashboard/admin/gallery/change_category_status.php', // this is the target
-        method: 'post', // method
-        data: {id: cat_id, status: disable}, // pass the input value to server
-        success: function(r) { // if the http response code is 200
-            alert(r)
-            $('#catid-'+cat_id+'-change-status-btn').clas
-            $('#cat-status-change').modal().hide();
-            $('.modal-backdrop').remove();
-            editing_cat_name = '';
-            editing_cat_id = '';
-            window.location = location.origin+"/dashboard/?page=categories&order=asc";
-        },
-        error: function(r) { // if the http response code is other than 200
-            alert(r);
-            HideSpinner();
-        }
-    });        
-});
-
 $('.cat-edit-form').on('click', function(e) {
     editing_cat_id = $(this).attr('id').substring(6);
     window.location = location.origin+"/dashboard?page=edit-category&id="+$(this).attr('id').substring(6);
@@ -105,30 +75,6 @@ $('#cancel-cat-create, #close-cat-create').on('click', function(e) {
     $("#new-cat-image-name").html("Escoger fichero...");
 });
 
-// Changing category status - enabled/disabled.
-$('.cat-status-change-form').on('click', function(e) {
-    // Getting category info.
-    editing_cat_id = $(this).attr('id');
-    editing_catname = $(this).attr('name');
-
-    // Setting action text.
-    action_title = 'Habilitando';
-    action_info = 'habilitar';
-    if ($('#'+editing_cat_id+'_cat_status').text().trim() == 'Habilitada') {
-        action_title = 'Desabilitando';
-        action_info = 'deshabilitar';
-    };
-    $('#staticBackdropLabel-statuschange').text(action_title+' categoría: "'+editing_catname+'"');
-    var text = '¿Estás seguro de que quieres '+action_info+' esta categoría?';
-    if (action_info == "deshabilitar") {
-        text = text + " Si la deshabilitas, no se borrará, pero dejará de estar visible en la galería hasta que la habilites de nuevo.";
-    } else {
-        text = text + " Si la habilitas, volverá a estar visible de nuevo en la galería.";
-    }
-    $('#statuschange_modal_info_text').text(text);
-    $('#cat-status-change').modal().show(); //Showing modal on top of website content.
-});
-
 // Reload page with new sorting order.
 $('#result-order').on('change', function(e) {
     value = $(this).children("option:selected").val();
@@ -139,12 +85,6 @@ $('#result-order').on('change', function(e) {
             break;
         case "desc":
             window.location = "?page=categories&order=desc";
-            break;
-        case "enabled":
-            window.location = "?page=categories&order=enabled";
-            break;
-        case "disabled":
-            window.location = "?page=categories&order=disabled";
             break;
     }
 });

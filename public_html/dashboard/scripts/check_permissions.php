2152694bf00116d57f7147fe5302113d05f8b103
Francisco Gálvez <afgalvezdiaz@gmail.com>
2022-10-30 15:21:48 +0100

    Update check_permissions.php

--- public_html/dashboard/scripts/check_permissions.php
+++ public_html/dashboard/scripts/check_permissions.php
@@ -4,14 +4,15 @@
         
         $pages = array 
             (
-              "show_categories" =>  "10000000",
-              "show_gallery" =>  "01000000",
-              "manage_categories" =>  "00100000",
-              "manage_gallery" =>  "00010000",
-              "manage_companySettings" =>  "00001000",
-              "manage_services" =>  "00000100",
-              "manage_users" =>  "00000010",
-              "manage_siteSettings" =>  "00000001"
+              "show_categories" =>  "100000000",
+              "show_gallery" =>  "010000000",
+              "manage_categories" =>  "001000000",
+              "manage_gallery" =>  "000100000",
+              "manage_companySettings" =>  "000010000",
+              "manage_services" =>  "000001000",
+              "manage_users" =>  "000000100",
+              "manage_siteSettings" =>  "000000010",
+              "standard_user" =>  "000000001"
             );
         
         $userPerm = 0;

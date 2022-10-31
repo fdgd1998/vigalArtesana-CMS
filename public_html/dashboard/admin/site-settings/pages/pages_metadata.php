f879cf3d5061fde74f2263e75546d0602d3cfbe3
Francisco Gálvez <afgalvezdiaz@gmail.com>
2022-10-31 12:41:27 +0100

    Code fixes

--- public_html/dashboard/admin/site-settings/pages/pages_metadata.php
+++ public_html/dashboard/admin/site-settings/pages/pages_metadata.php
@@ -31,7 +31,7 @@
 
 <!-- create category modal -->
 <div class="modal fade" id="page-edit" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="page-edit-title"></h5>
--- public_html/dashboard/admin/site-settings/pages/services.php
+++ public_html/dashboard/admin/site-settings/pages/services.php
@@ -35,7 +35,7 @@
 
 <!-- Delete category modal -->
 <div class="modal fade" id="delete-service-modal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="staticBackdropLabel-delete" >Borrando servicio</h5>

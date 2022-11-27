fb69e3a38a56f806ad7346d2bf2819429b00547a
Francisco Gálvez <afgalvezdiaz@gmail.com>
2022-11-27 16:08:09 +0100

    Update show_spinner.js

--- public_html/dashboard/js/show_spinner.js
+++ public_html/dashboard/js/show_spinner.js
@@ -1,4 +1,4 @@
 function ShowSpinnerOverlay (text) {
-var spinner_div = $("<div style='height:100vh; z-index: 999999 !important; position: fixed; bottom:0; left:0; right:0; background-color: rgba(255,255,255,.75)' class='row justify-content-center align-items-center' id='spinner-div'><div class='spinner-border' role='status'></div><span style='margin-left: 10px'>"+text+"</span></div>");
+var spinner_div = $("<div style='min-height: 100vh !important; z-index: 999999 !important; position: fixed; bottom:0; left:0; right:0; background-color: rgba(255,255,255,.75)' class='row justify-content-center align-items-center' id='spinner-div'><div class='spinner-border' role='status'></div><span style='margin-left: 10px'>"+text+"</span></div>");
     $("body").append(spinner_div);
 }
